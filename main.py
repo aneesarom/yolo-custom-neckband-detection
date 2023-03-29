d61c06a1b0851848ba3dd2b201aba5c7282536e4
aneesaro <speaktoanees@gmail.com>
2023-03-29 18:57:03 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import torch
+
+# Set the device
+dtype = torch.float
+device = torch.device("mps")
+
+
+# # yolo model creation
+# model = YOLO("yolo-weights/yolov8m.pt")
+# model.train(data="coco128.yaml", imgsz=320, batch=4, epochs=100, workers=0)
+
+# prediction
+model = YOLO("best.pt")
+results = model.predict("videos/3.mp4", save=True, conf=0.5, show=True)
+
+
